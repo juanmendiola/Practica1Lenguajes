9c00f1888fb2fc2c7796c6bbefd0fc649b742b63
JuanJoseVargasL <117392572+JuanJoseVargasL@users.noreply.github.com>
2024-04-02 01:34:05 -0500

    PitCrewTeam.java

--- PitCrewTeam.java
+++ PitCrewTeam.java
@@ -29,4 +29,23 @@ public class PitCrewTeam {
         System.out.println("Role: " + role);
         System.out.println("Number of members: " + members);
     }
+
+    public static void main(String[] args) {
+        // Create 20 members using role to classify
+        PitCrewTeam[] pitCrewMembers = new PitCrewTeam[20];
+        pitCrewMembers[0] = new PitCrewTeam("Tyre gunner", 4);
+        pitCrewMembers[1] = new PitCrewTeam("Tyre off", 4);
+        pitCrewMembers[2] = new PitCrewTeam("Tyre on", 4);
+        pitCrewMembers[3] = new PitCrewTeam("Front jack", 2);
+        pitCrewMembers[4] = new PitCrewTeam("Rear jack", 2);
+        pitCrewMembers[5] = new PitCrewTeam("Side jack", 2);
+        pitCrewMembers[6] = new PitCrewTeam("Front wing flap adjust", 2);
+        pitCrewMembers[7] = new PitCrewTeam("Lollipop man", 1);
+
+        // Print information of each member
+        for (PitCrewTeam member : pitCrewMembers) {
+            member.printInfo();
+            System.out.println();
+        }
+    }
 }
